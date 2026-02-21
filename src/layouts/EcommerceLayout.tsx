1f26fa801667479e8a0cac7249fc5d642fb9d4f9
Moahmed Zean <elkonmohamed911@gmail.com>
2026-02-21 10:38:10 +0200

    prevent admin & super admin from access to ecommerce site

--- src/layouts/EcommerceLayout.tsx
+++ src/layouts/EcommerceLayout.tsx
@@ -1,8 +1,19 @@
 import { Outlet } from "react-router-dom"
 import { Navbar } from "@/components/layout/Navbar"
 import { Footer } from "@/components/layout/Footer"
+import { useAuthStore } from "@/stores/useAuthStore"
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from "react"
 
 export function EcommerceLayout() {
+  const { role, isLoading } = useAuthStore()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isLoading && (role === 'admin' || role === 'superadmin')) {
+      navigate('/dashboard/dashHome')
+    }
+  } , [isLoading , navigate , role])
   return (
     <>
       <Navbar />
